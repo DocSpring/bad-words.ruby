715b6262b61b6a6c4d1db85611363993afbe0c61
hairyhum <hairyhum@gmail.com>
2013-03-17 01:14:45 +0400

    Update state.rb

--- lib/bad_word_detector/state.rb
+++ lib/bad_word_detector/state.rb
@@ -8,11 +8,17 @@ class State
     @length = 0
     @weight = 1
 
-    path.each do |rule|
+    path.each_index do |index|
      # puts item.inspect
+      rule = path[index]
       @text += rule.symbol
       @length += rule.length
-      @weight *= rule.weight
+      weight_multiplier = if index != 0 and path[index - 1].symbol == rule.symbol
+          1
+      else
+        rule.weight
+      end
+      @weight *= weight_multitplier
     end
   end
 
